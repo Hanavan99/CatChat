37f78525c8d973939fe8bed44a3956dbabb05c04
Hanavan99 <hanavan99@gmail.com>
2017-12-01 20:05:23 -0600

    Actually javadoced

--- src/catchat/client/Gui.java
+++ src/catchat/client/Gui.java
@@ -29,8 +29,9 @@ import catchat.core.Directory;
 import catchat.core.NetworkHandler;
 import catchat.server.Client;
 
-/*
+/**
  * Gui object for the Client
+ * 
  * @author Chuck Swisher
  */
 
@@ -50,15 +51,15 @@ public class Gui extends JFrame {
 	private ObjectInputStream input;
 	private Client client;
 
-	/*
-	 * Creates a new Gui using JFrame. 
-	 * Adds areas for the user to type, the chat history to be seen, and a button
-	 * to upload files to the server. An action handler is created to send text to
-	 * the client whenever enter is pressed. An action handler is also created for
-	 * the button to upload a file. It passed a file to the client object to be 
-	 * serialized and stored. Two JOptionPanes pop up; one to get the server IP
-	 * address and one for the user to select a handle. The various compenents
-	 * are added to the JFrame. The startRunning method is called.
+	/**
+	 * Creates a new Gui using JFrame. Adds areas for the user to type, the chat
+	 * history to be seen, and a button to upload files to the server. An action
+	 * handler is created to send text to the client whenever enter is pressed. An
+	 * action handler is also created for the button to upload a file. It passed a
+	 * file to the client object to be serialized and stored. Two JOptionPanes pop
+	 * up; one to get the server IP address and one for the user to select a handle.
+	 * The various compenents are added to the JFrame. The startRunning method is
+	 * called.
 	 */
 	public Gui() {
 		font1 = new Font("SansSerif", Font.BOLD, 15);
@@ -140,24 +141,24 @@ public class Gui extends JFrame {
 
 		userText.requestFocusInWindow();
 		userText.selectAll();
-		
+
 		ip = "";
 		boolean flag;
 		do {
 			flag = true;
 			ip = JOptionPane.showInputDialog("Enter \"104.236.244.255\" for Ubuntu sever or \"localhost\" to connect locally");
-			if(ip == null)
+			if (ip == null)
 				System.exit(0);
-			else if(ip.equals("104.236.244.255"))
+			else if (ip.equals("104.236.244.255"))
 				flag = false;
-			else if(ip.equals("localhost"))
+			else if (ip.equals("localhost"))
 				flag = false;
 		} while (flag);
-		
+
 		handle = "";
 		do {
 			handle = JOptionPane.showInputDialog("Enter your desired handle: ");
-			if(handle == null)
+			if (handle == null)
 				handle = "Handle not set";
 		} while (handle.equals(""));
 
@@ -167,11 +168,11 @@ public class Gui extends JFrame {
 		startRunning();
 	}
 
-	/*
-	 * Start running is a setup method to facilitate the rest of the class.
-	 * It calls connectToServer, setUpStreams, and whileChatting, all of
-	 * which are the meat and potatoes of the class. After those methods
-	 * end, the close method is called.
+	/**
+	 * Start running is a setup method to facilitate the rest of the class. It calls
+	 * connectToServer, setUpStreams, and whileChatting, all of which are the meat
+	 * and potatoes of the class. After those methods end, the close method is
+	 * called.
 	 */
 	public void startRunning() {
 		try {
@@ -187,20 +188,22 @@ public class Gui extends JFrame {
 		}
 	}
 
-	/*
-	 * Creates a new socket called connection using the ip from the
-	 * user and the port 12345.
-	 * @throws IOException if their is a problem creating a Socket.
+	/**
+	 * Creates a new socket called connection using the ip from the user and the
+	 * port 12345.
+	 * 
+	 * @throws IOException
+	 *             if their is a problem creating a Socket.
 	 */
 	private void connectToServer() throws IOException {
 		connection = new Socket(ip, 12345);
 	}
 
-	/*
+	/**
 	 * Creates new input and output streams using the socket created in
-	 * connectToServer. The streams facilitate communication between the
-	 * client and the sever. The streams and the users handle are used to 
-	 * create a client object.
+	 * connectToServer. The streams facilitate communication between the client and
+	 * the sever. The streams and the users handle are used to create a client
+	 * object.
 	 */
 	private void setUpStreams() throws IOException {
 		output = new ObjectOutputStream(connection.getOutputStream());
@@ -220,8 +223,7 @@ public class Gui extends JFrame {
 					} catch (IOException e) {
 						System.out.println("Error saving file");
 					}
-				}
-				else if(choice == JFileChooser.CANCEL_OPTION) {
+				} else if (choice == JFileChooser.CANCEL_OPTION) {
 					showMessage("\nYou pressed cancel.");
 				}
 			}
@@ -242,10 +244,12 @@ public class Gui extends JFrame {
 		});
 	}
 
-	/*
-	 * Constantly pulls events from the client to see if anything
-	 * has been received. Stops when closed.
-	 * @throws IOException if events cannot be reached
+	/**
+	 * Constantly pulls events from the client to see if anything has been received.
+	 * Stops when closed.
+	 * 
+	 * @throws IOException
+	 *             if events cannot be reached
 	 */
 	public void whileChatting() throws IOException {
 		ableToType(true);
@@ -254,22 +258,24 @@ public class Gui extends JFrame {
 		} while (true);
 	}
 
-	/*
-	 * Takes a string and sends it to the client to be sent to 
-	 * the server.
-	 * @param String message Text to be sent.
+	/**
+	 * Takes a string and sends it to the client to be sent to the server.
+	 * 
+	 * @param String
+	 *            message Text to be sent.
 	 */
 	private void sendMessage(String message) {
 		client.sendMessage(message);
 
 	}
 
-	/*
-	 * Takes a string and displays it to the JTextArea of the
-	 * JFrame. Creates a new Runnable specifically to update the
-	 * history so that the entire UI is not having to be constantly
-	 * rewritten.
-	 * @param final String TEXT String to be appended to the JTextArea.
+	/**
+	 * Takes a string and displays it to the JTextArea of the JFrame. Creates a new
+	 * Runnable specifically to update the history so that the entire UI is not
+	 * having to be constantly rewritten.
+	 * 
+	 * @param final
+	 *            String TEXT String to be appended to the JTextArea.
 	 */
 	public void showMessage(final String TEXT) {
 		SwingUtilities.invokeLater(new Runnable() {
@@ -279,11 +285,13 @@ public class Gui extends JFrame {
 		});
 	}
 
-	/*
-	 * Determines if the JTextArea and fileChooseButton can be used. If their
-	 * is no connection and the user tries to type and send a message, it causes
-	 * an error. This method is in place to make sure that error does not occur.
-	 * @param final Boolean TOF boolean expression of what to set the components to.
+	/**
+	 * Determines if the JTextArea and fileChooseButton can be used. If their is no
+	 * connection and the user tries to type and send a message, it causes an error.
+	 * This method is in place to make sure that error does not occur.
+	 * 
+	 * @param final
+	 *            Boolean TOF boolean expression of what to set the components to.
 	 */
 	private void ableToType(final Boolean TOF) {
 		SwingUtilities.invokeLater(new Runnable() {
@@ -294,10 +302,12 @@ public class Gui extends JFrame {
 		});
 	}
 
-	/*
+	/**
 	 * Closes the socket and stream and makes the user unable to interact with
 	 * components.
-	 * @throws IOException if the socket is unable to be closed.
+	 * 
+	 * @throws IOException
+	 *             if the socket is unable to be closed.
 	 */
 	private void close() {
 		showMessage("\nClosing streams and sockets. \nDISCONNECTED");
